4a3f6442c2c56453daf74733dd4093674b6dfe00
Ruzz-1010 <johnruzzelcorrea8@gmail.com>
2025-11-27 00:34:16 +0800

    fhffhf

--- server/models/User.js
+++ server/models/User.js
@@ -12,14 +12,12 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
-// Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
